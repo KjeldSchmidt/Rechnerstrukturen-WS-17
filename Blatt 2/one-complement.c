3ff4fd4f9863016d1edb985b4ebaeec34b35a8ec
Kjeld Schmidt <contact@kjeld-schmidt.com>
2017-05-03 22:57:39 +0200

    Solved 7)a)

--- Blatt 2/one-complement.c
+++ Blatt 2/one-complement.c
@@ -23,14 +23,25 @@ long convert_vector(bit_vector *b) {
 
   //  Teste, ob der uebergebene Vektor gueltig ist.
 
-  //  [Programmtext hier ergaenzen.]
+  if ( b == NULL || b->length != 32 ) {
+    perror("Invalider Bitvektor");
+  }
 
   //  Aktuelles (Zwischen-)Ergebnis.
   long value = 0;
+  unsigned char *position = b->bits;            // Set the pointer position to the first char in the bit array. We need this so we may increment carelessly, which is not possible with b->bits directly.
 
   //  Wandele um.
-  
-  //  [Programmtext hier ergaenzen.]
+
+  position++;
+
+  for ( int i = 1; i < b->length; ++i ) {       // Simply iterate over the bit array by length (Step 2 and 5 of the informal algorithm)
+    value *= 2;                 // It is important to _start_ by multiplying with two, because the last digit should _not_ be doubled. In the first iteration, this simply has no effect ( Step 3 )
+    value += *position;                         // Add the current positions value ( Step 4, part 2 ). Note that step 1 is needlessly includes one execution of step 4 - we don't do that.
+    position++;                                 // Increment the pointer to the next bit location. ( Step 4, part 1)
+  }
+
+  value -= *(b->bits) * INT_MAX;
 
   return value;
 }
@@ -50,7 +61,7 @@ bit_vector convert_number(long number) {
   //  Erzeuge Rueckgabe-Struktur.
   bit_vector b;
 
-  //  [Programmtext hier ergaenzen.]
+  memset(b.bits, FALSE, b.length);
 
   return b;
 }
