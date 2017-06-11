08069b61be5cc6bf79ea7e3bf6463c104a743d13
Kjeld Schmidt <contact@kjeld-schmidt.com>
2017-06-11 15:33:22 +0200

    guard against nonsensical input

--- Blatt 6/Aufgabe 19/main.c
+++ Blatt 6/Aufgabe 19/main.c
@@ -8,6 +8,16 @@
   Sie Ihren Programmtext vernuenftig. Danke.
 */
 
+void printNumber( char showBitField ) {
+	if ( showBitField >> 7 == 1 ) {
+		printf("This bitfield can't represent an actual number. You screwed up.\n");
+		return;
+	}
+
+
+
+}
+
 void showNumber(unsigned int number) {
   char numberAsString[4];
   sprintf( numberAsString, "%d", number );
