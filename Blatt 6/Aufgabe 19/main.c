917c356633098f006f2b3fad0f4e622fe3259019
Kjeld Schmidt <contact@kjeld-schmidt.com>
2017-06-11 15:20:17 +0200

    initialzed 19

---
+++ Blatt 6/Aufgabe 19/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/*
+  Hier Hilfsfunktionen ergaenzen. Bitte modularisieren
+  Sie Ihren Programmtext vernuenftig. Danke.
+*/
+
+void showNumber(unsigned int number) {
+  /*
+    Hier Programmtext ergaenzen.
+  */
+}
+
+int main(int argc, char **argv) {
+  showNumber(123);
+  showNumber(4567);
+  showNumber(8910);
+}
