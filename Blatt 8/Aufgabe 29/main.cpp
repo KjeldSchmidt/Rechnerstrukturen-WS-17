891ecacd61cfe1a324ad959655d3ad7f60ba6dcb
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-06-23 14:46:34 +0200

    all functions implemented, testing halfway done

--- Blatt 8/Aufgabe 29/main.cpp
+++ Blatt 8/Aufgabe 29/main.cpp
@@ -16,19 +16,42 @@ bool Bitvektor::checkSizes( const Bitvektor& other ) const {
 }
 
 Bitvektor Bitvektor::calculateAnd( const Bitvektor& other ) const {
+  if ( this->getSize() != other.getSize() ) return Bitvektor(0);
+  Bitvektor result( this->getSize() );
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    result.setBit( i, this->getBit( i ) & other.getBit( i ) );
+  }
 
+  return result;
 }
 
 Bitvektor Bitvektor::calculateOr( const Bitvektor& other ) const {
+  if ( this->getSize() != other.getSize() ) return Bitvektor(0);
+  Bitvektor result( this->getSize() );
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    result.setBit( i, this->getBit( i ) | other.getBit( i ) );
+  }
 
+  return result;
 }
 
 Bitvektor Bitvektor::calculateXor( const Bitvektor& other ) const {
+  if ( this->getSize() != other.getSize() ) return Bitvektor(0);
+  Bitvektor result( this->getSize() );
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    result.setBit( i, this->getBit( i ) != other.getBit( i ) );
+  }
 
+  return result;
 }
 
 Bitvektor Bitvektor::calculateNot() const {
+  Bitvektor result( this->getSize() );
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    result.setBit( i, ~this->getBit( i ) );
+  }
 
+  return result;
 }
 
 size_t Bitvektor::getSize() const {
@@ -36,19 +59,40 @@ size_t Bitvektor::getSize() const {
 }
 
 bool Bitvektor::getBit(size_t position) const {
-
+  if ( position < 0 || position >= this->getSize() ) {
+    throw "IndexOutOfBoundsException";
+  }
+  bool result = this->bits[ position ];
+  return result;
 }
 
 void Bitvektor::setBit(size_t position, bool value) {
-
+  if ( position < 0 || position >= this->getSize() ) {
+    throw "IndexOutOfBoundsException";
+  }
+  this->bits[ position ] = value;
 }
 
 bool Bitvektor::allZero() const {
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    if ( this->getBit( i ) ) {
+      return false;
+    }
+  }
 
+  return true;
 }
 
 bool Bitvektor::equals(const Bitvektor& other) const {
+  if ( this->getSize() != other.getSize() ) return false;
 
+  for ( int i = 0; i < this->getSize(); ++i ) {
+    if( this->getBit( i ) != other.getBit( i ) ) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void Bitvektor::printOnConsole() const {
--- Blatt 8/Aufgabe 29/tests.h
+++ Blatt 8/Aufgabe 29/tests.h
@@ -2,6 +2,7 @@
 
 
 void sizeTest() {
+  std::cout << "sizeTest" << std::endl;
   Bitvektor zeroLengthVektor( 0 );
   Bitvektor vektor2( 10 );
   Bitvektor vektor3( 10 );
@@ -25,7 +26,77 @@ void sizeTest() {
   std::cout << std::endl << std::endl;
 }
 
+void setAndGetTest() {
+  std::cout << "setAndGetTest" << std::endl;
+  Bitvektor vektor1( 10 );
+
+  vektor1.setBit( 5, 1 );
+  vektor1.setBit( 1, 0 );
+
+  bool result = false;
+  result = vektor1.getBit( 5 ) == 1;
+  std::cout << ( result == true );
+  result = vektor1.getBit( 0 ) == 0;
+  std::cout << ( result == true );
+
+  std::cout << std::endl << std::endl;
+}
+
+void allZeroTest() {
+  std::cout << "allZeroTest" << std::endl;
+  Bitvektor zeroLengthVektor( 0 );
+  Bitvektor vektor2( 10 );
+  vektor2.setBit(0, 1);
+  Bitvektor vektor3( 11 );
+
+  bool result = false;
+
+  result = zeroLengthVektor.allZero();
+  std::cout << ( result == true );
+  result = vektor2.allZero( );
+  std::cout << ( result == false );
+  result = vektor3.allZero();
+  std::cout << ( result == true);
+
+  std::cout << std::endl << std::endl;
+}
+
+void equalsTest() {
+  std::cout << "equalsTest" << std::endl;
+  Bitvektor zeroLengthVektor( 0 );
+  Bitvektor vektor1( 10 );
+  vektor1.setBit( 1, 1 );
+  Bitvektor vektor2( 10 );
+  vektor2.setBit( 1, 1 );
+  Bitvektor vektor3( 10 );
+  vektor3.setBit( 1, 0 );
+  Bitvektor vektor4( 11 );
+
+  bool result = false;
+
+  result = zeroLengthVektor.equals( vektor2 );
+  std::cout << ( result == false );
+  result = zeroLengthVektor.equals( zeroLengthVektor );
+  std::cout << ( result == true );
+  result = vektor2.equals( zeroLengthVektor );
+  std::cout << ( result == false );
+  result = vektor2.equals( vektor3 );
+  std::cout << ( result == false );
+  result = vektor2.equals( vektor2 );
+  std::cout << ( result == true );
+  result = vektor2.equals( vektor1 );
+  std::cout << ( result == true );
+  result = vektor2.equals( vektor4 );
+  std::cout << ( result == false );
+  result = vektor3.equals( vektor4 );
+  std::cout << ( result == false );
+
+  std::cout << std::endl << std::endl;
+}
 
 void runTests() {
   sizeTest();
+  setAndGetTest();
+  allZeroTest();
+  equalsTest();
 }
