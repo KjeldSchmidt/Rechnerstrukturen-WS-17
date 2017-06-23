86f6007c323c5f035b0290d90f7f6a5a74dcf262
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-06-23 13:07:22 +0200

    added constructor

--- Blatt 8/Aufgabe 29/main.cpp
+++ Blatt 8/Aufgabe 29/main.cpp
@@ -1,7 +1,12 @@
 #include "bitvektor.h"
+#include "tests.h"
 
 int main() {
   return 0;
+}
+
+Bitvektor::Bitvektor( size_t length ) {
+  this->bits = (bool*) malloc( length * sizeof( bits ) );
 }
 
 bool Bitvektor::checkSizes( const Bitvektor& other ) const {
---
+++ Blatt 8/Aufgabe 29/tests.h
@@ -0,0 +1,9 @@
+#include "bitvektor.h"
+
+void prepareTest() {
+
+}
+
+void sizeTest() {
+  //Bitvektor zeroLengthVektor.
+}
